a85785b8a14eba15de3e678f57d2c064fd0fff4a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-24 11:43:19 +0300

    works on left container styling

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -7,33 +7,33 @@ const Wrapper = () => {
     <main>
         <div className="main-container">
         <div className="left-container">
-            <div className="step-one">
-                <p className='one'>1</p>
+            <div className="steps">
+                <p className='number'>1</p>
                 <div className="info">
-                    <p>STEP 1</p>
-                    <p>YOUR INFO</p>
+                    <p className='info-steps'>STEP 1</p>
+                    <p className='my-info'>YOUR INFO</p>
                 </div>
             </div>
-            <div className="step-two">
-                <p className='two'>2</p>
-                <div className="plan">
-                    <p>STEP 2</p>
-                    <p>SELECT PLAN</p>
+            <div className="steps">
+                <p className='number'>2</p>
+                <div className="info">
+                    <p className='info-steps'>STEP 2</p>
+                    <p className='my-info'>SELECT PLAN</p>
                 </div>
             </div>
-            <div className="step-three">
-                <p className='3'>3</p>
-                <div className="add-ons">
-                    <p>STEP 3</p>
-                    <p>ADD-ONS</p>
+            <div className="steps">
+                <p className='number'>3</p>
+                <div className="info">
+                    <p className='info-steps'>STEP 3</p>
+                    <p className='my-info'>ADD-ONS</p>
                 </div>
 
             </div>
-            <div className="step-four">
-                <p className='4'>4</p>
-                <div className="summary">
-                    <p>STEP 4</p>
-                    <p>SUMMARY</p>
+            <div className="steps">
+                <p className='number'>4</p>
+                <div className="info">
+                    <p className='info-steps'>STEP 4</p>
+                    <p className='my-info'>SUMMARY</p>
                 </div>
             </div>
         </div>
