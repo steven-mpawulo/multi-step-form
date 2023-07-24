d887e3787b366b9125898446ce65ef4ea79b887e
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-24 15:13:22 +0300

    adds spacing between different card elements

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Arcade from '../images/icon-arcade.svg'
 import Advanced from '../images/icon-advanced.svg'
 import Pro from '../images/icon-pro.svg'
+import '../styles/plan.css'
 
 const Plan = () => {
   return (
@@ -10,26 +11,33 @@ const Plan = () => {
             <p>Select your plan</p>
             <p>You have the option of monthly or yearly billing.</p>
                 <div className="cards-plan-container">
-                    <div className="arcade-card">
+                    <div className="card-arcade">
                         <img src={Arcade} alt="arcade" />
                         <p>Arcade</p>
                         <p>&#36;9/mo</p>
                     </div>
-                    <div className="advanced-card">
+                    <div className="card-advanced">
                         <img src= {Advanced}alt="advanced-icon" />
                         <p>Advanced</p>
                         <p>&#36;12/mo</p>
                     </div>
-                    <div className="pro-card">
+                    <div className="card-pro">
                         <img src={Pro} alt="pro-icon" />
                         <p>Pro</p>
                         <p>&#36;15/mo</p>
                     </div>
                 </div>
                 <div className="for-switch">
-                    <span>Monthly</span>
+                    <div className="monthly">
+                    <span className='monthly'>Monthly</span>
+                    </div>
+                    <div className="switch-container">
                     <input type="checkbox" name="switch" id="switch" />
-                    <span>Yearly</span>
+                    </div>
+                    <div className="yearly">
+                    <span className='yearly'>Yearly</span>
+                    </div>
+                   
                 </div>
         </div>
     </section>
