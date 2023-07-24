7fa5985bcd251b0c2b1d027b3ef14608643c2292
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-24 12:09:01 +0300

    styles text and input fields in right container

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -6,18 +6,18 @@ const PersonalInfo = () => {
         <section>
             <div className="personal-info-container">
                 <p className='personal-info-text'> Personal info</p>
-                <p>Please provide your name, email address and phone number</p>
+                <p className='personal-info-supplementary'>Please provide your name, email address and phone number</p>
                 <div className="name">
                     <label htmlFor="name">Name</label> <br />
-                    <input type="text" name='name' id='name' />
+                    <input type="text" name='name' id='name' placeholder='eg.stephen King' />
                 </div>
                 <div className="email">
                     <label htmlFor="email">Email Address</label> <br />
-                    <input type="email" name="email" id="email" />
+                    <input type="email" name="email" id="email" placeholder='eg.stephenking@lorem.com'/>
                 </div>
                 <div className="phone-number">
                     <label htmlFor="phone-number">Phone Number</label> <br />
-                    <input type="tel" name="phone-number" id="phone-number" />
+                    <input type="tel" name="phone-number" id="phone-number" placeholder='eg.+1 2345678'/>
                 </div>
             </div>
         </section>
