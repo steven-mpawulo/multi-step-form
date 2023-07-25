a46cbb2f92b23c2b9255cde378cf240d569d5bd3
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-25 10:07:21 +0300

    adds styling to first online service container

--- src/components/PickAddOns.js
+++ src/components/PickAddOns.js
@@ -1,27 +1,36 @@
 import React from 'react'
+import '../styles/pick_add_ons.css'
 
 const PickAddOns = () => {
   return (
     <div className="pick-add-ons-container">
-        <p>Pick add-ons</p>
-        <p>Add ons help enhance your gaming experience.</p>
+        <p className='pick-add-ons-text'>Pick add-ons</p>
+        <p className='pick-add-ons-supplementary-text'>Add ons help enhance your gaming experience.</p>
         <div className="add-ons-container">
             <div className="online-service">
+                <div className="first-inner-container">
                 <input type="checkbox" />
-                <div className="online-service-inner-container">
+                </div>
+                <div className="second-inner-container">
                     <p>Online service</p>
                     <p>Access to multiplayer games</p>
                 </div>
-                <p>&#36;1/mo</p>
+                <div className="third-inner-container">
+                <span>&#36;1/mo</span>
+                </div>
             </div>
         </div>
         <div className="larger-storage-container">
+            <div className="first-inner-container">
             <input type="checkbox" />
-            <div className="larger-storage-inner-container">
+            </div>
+            <div className="second-inner-container">
                 <p>Larger storage</p>
                 <p>Extra 1TB of cloud save</p>
             </div>
+            <div className="third-inner-container">
             <p>&#36;2/mo</p>
+            </div>
         </div>
         <div className="customizable-profile-container">
             <input type="checkbox" />
